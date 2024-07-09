c48217069f714d5b6a2456dfe227eb6fae747d63
Yash Tyagi <contactyashtyagi@gmail.com>
2024-07-10 00:04:13 +0530

    learned about variables

--- Chapter-1-(Basics)/JavaBasics.java
+++ Chapter-1-(Basics)/JavaBasics.java
@@ -1,8 +1,17 @@
+// Boilerplate Code
 public class JavaBasics{
     public static void main(String args[]){
         System.out.println("Yash Tyagi!"); // println - add next line in the end of the string
         System.out.print("Yash Tyagi!"); // print - simply print the string
+
+        // Variables - used to store data in them
+        int a = 4;
+        int b = 5;
+        String name = "Yash Tyagi";
+    
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(name);
     }
 }
 
-// Boilerplate Code
---
+++ Chapter-1-(Basics)/PrintPattern.java
@@ -0,0 +1,20 @@
+/*
+    print this pattern without using loops, just use Print
+
+    pattern : 
+    ****
+    ***
+    **
+    *
+    
+*/ 
+
+
+public class PrintPattern{
+    public static void main(String args[]){
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+}
