5a2f67e3a06c23e27d0b14e5510cf8727c3f700e
Yash Tyagi <contactyashtyagi@gmail.com>
2024-07-09 20:03:33 +0530

    Java basic syntax and output

---
+++ Chapter-1-(Basics)/JavaBasics.java
@@ -0,0 +1,8 @@
+public class JavaBasics{
+    public static void main(String args[]){
+        System.out.println("Yash Tyagi!"); // println - add next line in the end of the string
+        System.out.print("Yash Tyagi!"); // print - simply print the string
+    }
+}
+
+// Boilerplate Code
